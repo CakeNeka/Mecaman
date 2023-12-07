d666c1270bffd751775a920f8c9e253184efea7c
Leo0756 <113984558+Leo0756@users.noreply.github.com>
2023-12-07 10:54:08 +0100

    Palabras de emergencia - RandomWordGenerator

--- src/mecaman/wordgeneration/RandomWordGenerator.java
+++ src/mecaman/wordgeneration/RandomWordGenerator.java
@@ -10,13 +10,19 @@ public class RandomWordGenerator {
     private static RandomWordGenerator instance;
     private final String FILE_NAME = "resources/WordList.csv";
     private final Path FILE_PATH = Path.of(FILE_NAME).toAbsolutePath();
-    private final List<String> wordList;
+    private List<String> wordList;
 
-    private RandomWordGenerator() throws IOException {
-        wordList = Files.readAllLines(FILE_PATH);
+    private RandomWordGenerator() {
+        try {
+            wordList = Files.readAllLines(FILE_PATH);
+        } catch (IOException e) {
+            System.err.println("No se ha encontrado el archivo, utilizando PALABRAS DE EMERGENCIA");
+            // Si no se encuentra el archivo, se utilizan estas palabras
+            wordList = Arrays.asList("Mar", "Amor", "Conciencia", "MECAMAN", "TYPEMAN");
+        }
     }
 
-    public static RandomWordGenerator getInstance() throws IOException {
+    public static RandomWordGenerator getInstance() {
         if (instance == null) {
             instance = new RandomWordGenerator();
         }
